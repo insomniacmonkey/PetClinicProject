c014743be2549f432d3bc257ea466af7772e4a85
Chanse Strode <chansestrode@gmail.com>
2022-12-23 22:56:42 -0600

    Home page automated

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Welcome page', () => {
+  it('displays the welcome message and images', () => {
     cy.visit('http://3.238.20.218:8080/')
+
+    // Check the welcome message
+    cy.get('h2').should('have.text', 'Welcome')
+
+    // Check the first image
+    cy.get('.img-responsive').should('have.attr', 'src', '/resources/images/pets.png')
+
   })
 })
